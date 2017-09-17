57b2ab25a233716bf18c80ec3ee10fc4a6f54b5a
Aleksandrs Ivanovs <aivanovs@twilio.com>
2017-09-17 10:47:29 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const express = require('express');
+const config = require('./configuration.json');
+const TokenProvider = require('./token-provider');
+const bodyParser = require('body-parser');
+const ngrok = require('ngrok');
+const cors = require('cors');
+
+const app = express();
+
+app.set('json spaces', 2);
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors());
+
+app.get('/configuration', function(req, res) {
+  if (config) {
+    res.json(config);
+  } else {
+    res.json({});
+  }
+});
+
+app.get('/token', function(req, res) {
+  if (req.query.identity) {
+    res.send(TokenProvider.getToken(req.query.identity, req.query.pushChannel));
+  } else {
+    throw new Error('no `identity` query parameter is provided');
+  }
+});
+
+app.listen(3002, function() {
+  console.log('Token provider listening on port 3002!');
+
+  var ngrokOptions = {
+    proto: 'http',
+    addr: 3002
+  };
+  if (config.ngrokSubdomain) {
+    ngrokOptions.subdomain = config.ngrokSubdomain
+  }
+
+  ngrok.once('connect', function(url) {
+    console.log('ngrok url is ' + url);
+  });
+  ngrok.connect(ngrokOptions);
+});
+
+
+
